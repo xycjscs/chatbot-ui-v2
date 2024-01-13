055e01b4d700587b9be026f3ac69efc7d534addc
xycjscs <yuchao-xu@hotmail.com>
2024-01-13 23:30:12 +0800

    优化布局

--- components/sidebar/sidebar-switcher.tsx
+++ components/sidebar/sidebar-switcher.tsx
@@ -64,7 +64,7 @@ export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
         /> */}
       </TabsList>
 
-      <div className="flex flex-col items-center space-y-4">
+      <div className="absolute bottom-20 flex flex-col items-center space-y-4">
         {/* TODO */}
         {/* <WithTooltip display={<div>Import</div>} trigger={<Import />} /> */}
 
