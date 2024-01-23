c0d4ab2ccfb1f8809f1067fe7fdcb63424244884
xycjscs <yuchao-xu@hotmail.com>
2024-01-23 23:28:23 +0800

    测试 vercel edge 影响

--- app/api/chat/deepseek/route.ts
+++ app/api/chat/deepseek/route.ts
@@ -5,7 +5,7 @@ import { OpenAIStream, StreamingTextResponse } from "ai"
 import OpenAI from "openai"
 import { ChatCompletionCreateParamsBase } from "openai/resources/chat/completions.mjs"
 
-export const runtime = "edge"
+//export const runtime = "edge"
 
 export async function POST(request: Request) {
   const json = await request.json()
