885563ff5a53a82b2bdb6f95c137cab5e5d6de67
xycjscs <yuchao-xu@hotmail.com>
2024-01-26 16:38:13 +0800

    default gpt 3.5

--- components/chat/chat-hooks/use-chat-handler.tsx
+++ components/chat/chat-hooks/use-chat-handler.tsx
@@ -112,7 +112,7 @@ export const useChatHandler = () => {
     } else if (selectedWorkspace) {
       setChatSettings({
         model: (selectedWorkspace.default_model ||
-          "gpt-4-1106-preview") as LLMID,
+          "gpt-3.5-turbo-1106") as LLMID,
         prompt:
           selectedWorkspace.default_prompt ||
           "You are a friendly, helpful AI assistant.",
