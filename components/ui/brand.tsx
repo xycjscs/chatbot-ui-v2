9bf8d885e541b512d066a7379d4b8fd2459a3cce
xycjscs <yuchao-xu@hotmail.com>
2024-01-28 15:31:43 +0800

    update url

--- components/ui/brand.tsx
+++ components/ui/brand.tsx
@@ -12,7 +12,7 @@ export const Brand: FC<BrandProps> = ({ theme = "dark" }) => {
   return (
     <Link
       className="flex cursor-pointer flex-col items-center hover:opacity-50"
-      href="https://www.miml.com"
+      href="https://www.miml.org"
       target="_blank"
       rel="noopener noreferrer"
     >
