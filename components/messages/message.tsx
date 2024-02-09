ac6e7c66ac60a892cc120215522ea95708959f00
xycjscs <yuchao-xu@hotmail.com>
2024-02-09 14:05:32 +0800

    优化背景颜色

--- components/messages/message.tsx
+++ components/messages/message.tsx
@@ -156,7 +156,7 @@ export const Message: FC<MessageProps> = ({
     <div
       className={cn(
         "flex w-full justify-center",
-        message.role === "user" ? "" : "" //"bg-secondary"
+        message.role === "user" ? "" : "bg-white bg-opacity-15" //"bg-secondary"
       )}
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
