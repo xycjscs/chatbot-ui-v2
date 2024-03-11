e1740ae2ea7bf96bc45e7e4cb1435d21aa2cec96
xycjscs <yuchao-xu@hotmail.com>
2024-03-11 14:12:53 +0800

    流式输出乱码问题

--- components/chat/chat-helpers/index.ts
+++ components/chat/chat-helpers/index.ts
@@ -334,7 +334,7 @@ export const processResponse = async (
               const updatedChatMessage: ChatMessage = {
                 message: {
                   ...chatMessage.message,
-                  content: chatMessage.message.content + contentToAdd
+                  content: fullText //chatMessage.message.content + contentToAdd
                 },
                 fileItems: chatMessage.fileItems
               }
